97e0bde56f5108cce35654d816ee50aeaccbd638
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-09-27 13:39:23 -0500

    String Operators in javascript

---
+++ StringOperator.js
@@ -0,0 +1,10 @@
+
+
+    //String Operators
+
+    let age = 25;
+    let info = "My name is Caro, I am " + age + " years old."
+    console.log(info);
+
+    let numbers = 4 + " " + 5; // A way to create space in between numbers that aren't strings.
+    console.log(numbers);
