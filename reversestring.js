6a0be5feafff884ec80523e880870e72bc50f807
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-10-08 19:36:19 -0500

    String reversed

---
+++ reversestring.js
@@ -0,0 +1,3 @@
+const reverse = str => str.split('').reverse().join(''); //.join joins the words in the right order, without it, it just outputs each word separately bc of .split
+
+reverse('Word in reverse'); // outputs 'esrever ni droW'
