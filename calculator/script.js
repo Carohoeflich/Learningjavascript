82cb47ca462c8ed1bccc42701787eb67266bba21
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-10-09 22:23:02 -0500

    Calculator works, except decimals, still WIP

--- calculator/script.js
+++ calculator/script.js
@@ -8,11 +8,11 @@ class Calculator {
     clear(){
         this.currentOperand = ''
         this.previousOperand = ''
-        this.operation = undefined
+        this.operation = ''
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
     }
 
     appendNumber(number){
@@ -23,15 +23,53 @@ class Calculator {
     }
 
     chooseOperation(operation){
-
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute() //this is so, if we chose another operator later on, it computes the first 2 things we wanted and adds the second operator
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute(){
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return //if there's no number then it cancels the function completely
+        switch (this.operation){
+            case '+':
+                computation = prev + current
+            break;
+            case '-':
+                computation = prev - current
+            break;
+            case '*':
+                computation = prev * current
+            break;
+            case '÷':
+                computation = prev / current
+            break;
+        default: return
+        }
 
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber(number){  //so the numbers display commas every 3 numbers
+        const floatNumber = parseFloat(number)
+        if(isNaN(floatNumber)) return ''
+        return floatNumber.toLocaleString('en') //en for english
     }
 
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber (this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)}${this.operation}`;
+        }
     }
 }
 
@@ -57,4 +95,19 @@ operationButtons.forEach(button => {
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button =>{
+    calculator.delete()
+    calculator.updateDisplay()
 })
