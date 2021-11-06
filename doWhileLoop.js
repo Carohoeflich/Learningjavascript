a83318ad055fc70e2ebb45aded2eb699d4154d92
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-11-06 14:12:25 -0600

    Do While Loops explained with a few examples

---
+++ doWhileLoop.js
@@ -0,0 +1,12 @@
+do {
+    // loop body
+  } while (condition);
+
+
+
+  let i = 0;
+do {
+  console.log( i );
+  i++;
+} while (i < 3);
+// this shows 0, 1 and 2 because we said while its less than 3.
