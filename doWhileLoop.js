108bd6de1fa2009e373dd1eb9a37f77910f123fd
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-11-13 11:22:41 -0600

    traducido al espanol

--- doWhileLoop.js
+++ doWhileLoop.js
@@ -1,5 +1,6 @@
 do {
     // loop body
+    //codigo
   } while (condition);
 
 
@@ -10,3 +11,4 @@ do {
   i++;
 } while (i < 3);
 // this shows 0, 1 and 2 because we said while its less than 3.
+// Esto muestra 0, 1 y 2 porque le dijimos que mientras fuera menor a 3.
