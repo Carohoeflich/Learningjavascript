9cd851c304c5740e5c2c46de81aef3d748806df0
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-11-15 15:46:32 -0600

    Traduci al espanol

--- dayoftheyear.js
+++ dayoftheyear.js
@@ -1,4 +1,5 @@
 //what day of the year is today
+//Forma de saber que dia del año es hoy
 const dayOfYear = (date) =>
   Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
 
